244818c724d8b1c127a3f2b6a078b39187539b7b
LI, Qi <goodlq11@gmail.com>
2019-11-08 17:31:35 -0500

    dsa

--- draft.py
+++ draft.py
@@ -1,79 +1,43 @@
-from pyspark.sql import functions
-from dateutil import parser
-datasets = spark.read.format('csv').options(inferschema='true', sep='\t').load(
-    '/user/hm74/NYCOpenData/datasets.tsv').toDF('filename', 'title')
+from to_date_robust import to_date_robust
 
-filename = '7299-2etw'
-dataset = spark.read.format('csv').options(header='true', inferschema='true', sep='\t').load(
-    '/user/hm74/NYCOpenData/{}.tsv.gz'.format(filename))
-print(filename.encode('utf-8'), title.encode('utf-8'))
+datasets = (spark.read.format('csv')
+            .options(inferschema='true', sep='\t')
+            .load('/user/hm74/NYCOpenData/datasets.tsv')
+            .toDF('filename', 'title'))
 
 number_non_empty_cells = dataset.select([count(c) for c in dataset.columns])
 number_empty_cells = dataset.select([count(when (col(c).isNull(), c)).alias(c) for c in dataset.columns])
 number_distinct_values = dataset.select([countDistinct(c).alias(c) for c in dataset.columns])
 frequent_values = [dataset.groupBy(c).count().orderBy(desc('count')).limit(5).select(c) for c in dataset.columns]
 
-
-def to_iso_string(x):
-    try:
-        return parser.parse(x).isoformat()
-    except:
-        return None
-
-
-to_iso_string_udf = functions.udf(to_iso_string)
-
-
-def to_date_robust(x): return functions.to_date(to_iso_string_udf(x))
-
-
-dataset.select(to_date_robust(dataset['Day & Date'])).show()
-
-# to_date_robust
-
 ret = dataset.describe().collect()
 ret = dataset.select(dataset.xx).distinct().count()
 
+filename = 'sm48-t3s6'
 for filename, title in datasets.rdd.toLocalIterator():
+    print('>> entering {}.tsv.gz: {}'.encode('utf-8'), filename, title)
+    dataset = (spark.read.format('csv')
+               .options(header='true', inferschema='true', sep='\t')
+               .load('/user/hm74/NYCOpenData/{}.tsv.gz'.format(filename)))
     output = {'dataset_name': '', 'columns': [], 'key_column_candidates': []}
-    column = {
-        'column_name': '',
-        'number_non_empty_cells': 0,
-        'number_empty_cells': 0,
-        'number_distinct_values': 0,
-        'frequent_values': [],
-        "data_types": [
-            {
-                "type": "INTEGER (LONG)",
-                "count": 0,
-                "max_value": 0,
-                "min_value": 0,
-                "mean": 0,
-                "stddev": 0,
-            },
-            {
-                "type": "REAL",
-                "count": 0,
-                "max_value": 0,
-                "min_value": 0,
-                "mean": 0,
-                "stddev": 0,
-            },
-            {
-                "type": "DATE/TIME",
-                "count": 0,
-                "max_value": '',
-                "min_value": '',
-            },
-            {
-                "type": "TEXT",
-                "count": 0,
-                "shortest_values": [],
-                "longest_values": [],
-                "average_length": 0,
-            },
-        ],
-        "semantic_types": [
-            {"semantic_type": '',  "count": 0},
-        ],
-    }
+    output['dataset_name'] = filename
+    for column in dataset.schema:
+        name = column.name
+        dataType = column.dataType
+        column_output = {
+            'column_name': '',
+            'number_non_empty_cells': 0,
+            'number_empty_cells': 0,
+            'number_distinct_values': 0,
+            'frequent_values': [],
+            "data_types": [],
+            "semantic_types": [
+                {"semantic_type": '',  "count": 0},
+            ],
+        }
+        column_output['column_name'] =name
+        column_output['number_non_empty_cells'] = None
+        column_output['number_empty_cells'] = None
+        column_output['number_distinct_values'] = None
+        column_output['frequent_values'] = None
+        output['columns'].append(column_output)
